2476e8b86ba1d08d765984e8ef45afd057d1315e
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-20 15:36:57 +0200

    add seeder with faker, edit l extension of dashboard

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newPost = new Post();
+            $newPost->title = $faker->words(4, true);
+            $newPost->slug = Str::of($newPost->title)->slug('-');
+            $newPost->image_cover = $faker->imageUrl(400, 400, 'Posts', true, $newPost->title, true, 'png');
+            $newPost->description = $faker->paragraph();
+            $newPost->save();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-/* Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
- */
+    });
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,4 +34,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
